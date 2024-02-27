a976402bfe3016d59a97bfe6665407eebcc7bc34
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2024-02-27 14:06:27 +0000

    local player emits an event whenever it moves

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,9 +6,12 @@ namespace TerrariaRipoffNNF.scripts;
 
 public partial class Player : CharacterBody2D {
     private static Player LocalPlayer { get; set; }
-    
+
     [Export] private MultiplayerSynchronizer multiplayerSynchronizer;
     [Export] private float speed = 300f;
+    
+    private int XCoords => (int)Math.Round(Position.X / WorldManager.Instance.BlockSize);
+    private int YCoords => (int)Math.Round(Position.Y / WorldManager.Instance.BlockSize);
 
     [Signal]
     public delegate void LocalPlayerEnteredLocationEventHandler(
@@ -19,38 +22,40 @@ public partial class Player : CharacterBody2D {
     public override void _EnterTree() {
         int peerId = Name.ToString()!.ToInt();
         multiplayerSynchronizer.SetMultiplayerAuthority(peerId);
-        
+
         if (peerId == Multiplayer.GetUniqueId()) {
             LocalPlayer = this;
             InputManager.Instance.HorizontalInputChanged += newInput => horizontalInput = newInput;
-            // EmitSignal(SignalName.LocalPlayerEnteredLocation, 0, 0);
         }
 
         PlayerManager.Instance.CreatedPlayerOnServer += (xSpawnCoords, ySpawnCoords) => {
             int blockSize = WorldManager.Instance.BlockSize;
+            
+            // reconsider this, it requires duplication everywhere
             Position = new Vector2(xSpawnCoords * blockSize, ySpawnCoords * blockSize);
+            
+            EmitSignal(SignalName.LocalPlayerEnteredLocation, XCoords, YCoords);
         };
-
     }
-    
-    
-
 
     public override void _Process(double delta) {
         if (this != LocalPlayer) return;
 
+        (int previousXCoords, int previousYCoords) = (XCoords, YCoords);
         Position += new Vector2((float)delta * speed * horizontalInput, 0);
+
+        if (previousXCoords != XCoords || previousYCoords != YCoords) {
+            EmitSignal(SignalName.LocalPlayerEnteredLocation, 
+                XCoords, YCoords, previousXCoords, previousYCoords);
+        }
         
     }
 }
 
 /*
-
-
  *  when Player Spawned
  *  when Player Moved
  *      emit event with position
  *      Rpc that player, send server data: list of SavedBlocks,
  *      On retrieval: check existing blocks nearby, spawn as appropriate
-
 */
