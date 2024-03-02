3bb6b5af150f7ebef89ed9812b468bbda8c2dfa7
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2024-03-02 13:15:45 +0000

    reorganised

--- GameManagers/Scripts/GameManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using Godot;
-
-namespace TerrariaRipoffNNF.GameManagers.Scripts; 
-
-public abstract partial class GameManager : Node {
-    public const int HOST_ID = 1;
-    public static GameManager Instance { get; protected set; }
-}
