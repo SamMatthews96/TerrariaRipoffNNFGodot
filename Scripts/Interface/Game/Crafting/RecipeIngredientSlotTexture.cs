2fb0a07e2558f91d002dbfce8dbc40cd6034f796
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2024-12-17 22:48:57 +0000

    added torch temp recipe

--- Scripts/Interface/Game/Crafting/RecipeIngredientSlotTexture.cs
+++ Scripts/Interface/Game/Crafting/RecipeIngredientSlotTexture.cs
@@ -3,7 +3,8 @@
 namespace TerrariaRipoffNNF;
 
 public partial class RecipeIngredientSlotTexture : TextureRect {
-    
+    private Item _selectedIngredientItem;
+    [Export] private PopupPanel _ingredientPopupPanel;
     
     public static RecipeIngredientSlotTexture Create(RecipeIngredientSlot ingredient) {
         RecipeIngredientSlotTexture newTexture =
@@ -12,5 +13,16 @@ public partial class RecipeIngredientSlotTexture : TextureRect {
         
         newTexture.Texture = ingredient.Icon;
         return newTexture;
+    }
+
+    public override void _Ready() {
+        _ingredientPopupPanel.Hide();
+        MouseEntered += () => {
+            // _ingredientPopupPanel.Show();
+        };
+        MouseExited += () => {
+            // _ingredientPopupPanel.Hide();
+        };
+
     }
 }
---
+++ Scripts/Resources/Recipes/RecipeStatic.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Godot;
+using Godot.Collections;
+
+namespace TerrariaRipoffNNF;
+
+[GlobalClass]
+public partial class RecipeStatic : Recipe {
+    [Export] private Item _resultItem;
+    
+    public override Item Build(Dictionary<string, Item> suppliedIngredients) {
+        return _resultItem;
+    }
+}
