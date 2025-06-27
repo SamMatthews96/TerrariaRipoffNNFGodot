4f392b82dfd3cc600fe91d84c958e6f97bede8b7
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2025-06-28 00:16:13 +0100

    added basic recipes

--- Scripts/Resources/Item/ItemIngredient.cs
+++ Scripts/Resources/Item/ItemIngredient.cs
@@ -8,7 +8,17 @@ public partial class ItemIngredient : ItemProperty {
     [Export] public IngredientType IngredientType { get; private set; }
     [Export] public float Quality { get; private set; }
     [Export] public string Name { get; private set; }
-    
+
+    public static ItemIngredient Create(
+        IngredientType type, float quality, string name
+    ) {
+        return new ItemIngredient {
+            IngredientType = type,
+            Quality = quality,
+            Name = name
+        };
+    }
+
     public override Dictionary GetTooltipAttributes() {
         return new Dictionary();
     }
---
+++ Scripts/Resources/Recipes/ItemIngredientPropertyOutputTemplate.cs
@@ -0,0 +1,22 @@
+﻿using Godot;
+using Godot.Collections;
+
+namespace TerrariaRipoffNNF;
+
+[GlobalClass]
+public partial class ItemIngredientPropertyOutputTemplate : ItemPropertyOutputTemplate {
+    [Export] public IngredientType IngredientType { get; private set; }
+    [Export] public RecipePropertyMapMultiplier Quality { get; private set; }
+    [Export] public RecipePropertyMapString Name { get; private set; }
+
+    public override ItemProperty Build(
+        Dictionary<string, Item> suppliedIngredients,
+        Dictionary<string, RecipeIngredientSlot> ingredientSlots
+    ) {
+        return ItemIngredient.Create(
+            type: IngredientType,
+            quality: Quality.ResolveTemplate(suppliedIngredients, ingredientSlots),
+            name: Name.ResolveTemplate(suppliedIngredients, ingredientSlots)
+        );
+    }
+}
