6ccef02a57d7b65f2f02001062824adf3eb21ec6
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-04-17 21:20:10 +0100

    deleted old scenes

--- Scripts/GameObjects/WorldObject/PickupEntity.cs
+++ Scripts/GameObjects/WorldObject/PickupEntity.cs
@@ -1,7 +1,11 @@
 ﻿using Godot;
+using TerrariaRipoffNNF.Scripts.GameObjects;
 
 namespace TerrariaRipoffNNF;
 
-public partial class PickupEntity : Node2D {
+public partial class PickupEntity : Node2D, IEntity {
+    [Export] private Sprite2D _pickupSprite;
+    private Item _item;
+    public Vector2I CellCoordinates;
     
 }
