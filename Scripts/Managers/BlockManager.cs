01916a17065e7f773c34587c664df917fa13a5ca
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-05-03 18:23:04 +0100

    optimized multiplayer sync when client joins

--- Scripts/Managers/BlockManager.cs
+++ Scripts/Managers/BlockManager.cs
@@ -16,15 +16,13 @@ public partial class BlockManager : Node2D {
     public event Action<Vector2I> BlockCreated;
     public event Action<Vector2I, ushort> WallDestroyed;
 
-    private const int ChunkSize = 50;
-
     public override void _Ready() {
         Blocks = new Block[_world.WorldSize.X, _world.WorldSize.Y];
         Walls = new Block[_world.WorldSize.X, _world.WorldSize.Y];
 
         if (!_world.IsHost) return;
 
-        Dictionary<int, Array> savedBlocks = 
+        Dictionary<int, Array> savedBlocks =
             (Dictionary<int, Array>)_world.WorldData["blocks"];
         foreach ((int itemId, Array cellArray) in savedBlocks) {
             foreach (Array cell in cellArray) {
@@ -37,7 +35,7 @@ public partial class BlockManager : Node2D {
             }
         }
 
-        Dictionary<int, Array> savedWalls = 
+        Dictionary<int, Array> savedWalls =
             (Dictionary<int, Array>)_world.WorldData["walls"];
         foreach ((int itemId, Array cellArray) in savedWalls) {
             foreach (Array cell in cellArray) {
@@ -48,13 +46,10 @@ public partial class BlockManager : Node2D {
                     ItemId = (ushort)itemId
                 };
             }
-            
         }
 
         _world.PlayerManager.PlayerSpawnedOnHost += OnPlayerSpawnedOnHost;
-        TreeExiting += () => {
-            _world.PlayerManager.PlayerSpawnedOnHost -= OnPlayerSpawnedOnHost;
-        };
+        TreeExiting += () => { _world.PlayerManager.PlayerSpawnedOnHost -= OnPlayerSpawnedOnHost; };
     }
 
     public void ClientGetWorldData() {
@@ -136,90 +131,74 @@ public partial class BlockManager : Node2D {
     private void RpcRequestWorldData() {
         int requestingPeerId = Multiplayer.GetRemoteSenderId();
 
-        // Calculate chunks
-        int chunksX = (int)Math.Ceiling((double)_world.WorldSize.X / ChunkSize);
-        int chunksY = (int)Math.Ceiling((double)_world.WorldSize.Y / ChunkSize);
-        int totalChunks = chunksX * chunksY;
+        Dictionary data = new() {
+            ["blocks"] = SerializeChunk(Blocks),
+            ["walls"] = SerializeChunk(Walls)
+        };
 
-        // Send chunks
-        int chunkIndex = 0;
-        for (int chunkX = 0; chunkX < chunksX; chunkX++) {
-            for (int chunkY = 0; chunkY < chunksY; chunkY++) {
-                Array blockData = SerializeChunk(chunkX, chunkY, Blocks);
-                Array wallData = SerializeChunk(chunkX, chunkY, Walls);
-                
-                Dictionary chunkPacket = new() {
-                    ["chunkX"] = chunkX,
-                    ["chunkY"] = chunkY,
-                    ["chunkIndex"] = chunkIndex,
-                    ["totalChunks"] = totalChunks,
-                    ["blocks"] = blockData,
-                    ["walls"] = wallData
-                };
-
-                RpcId(requestingPeerId, nameof(RpcProcessWorldChunk),
-                    chunkPacket);
-                chunkIndex++;
-            }
-        }
+        RpcId(requestingPeerId, nameof(RpcProcessWorldChunk),
+            data);
     }
 
-    private Array SerializeChunk(int chunkX, int chunkY, Block[,] data) {
-        Array chunkEntities = new();
+    private Dictionary<ushort, Array> SerializeChunk(Block[,] data) {
+        Dictionary<ushort, Array> groupedByItemId = new();
 
-        int startX = chunkX * ChunkSize;
-        int startY = chunkY * ChunkSize;
-        int endX = Math.Min(startX + ChunkSize, _world.WorldSize.X);
-        int endY = Math.Min(startY + ChunkSize, _world.WorldSize.Y);
-
-        for (int x = startX; x < endX; x++) {
-            for (int y = startY; y < endY; y++) {
+        for (int x = 0; x < _world.WorldSize.X; x++) {
+            for (int y = 0; y < _world.WorldSize.Y; y++) {
                 Block block = data[x, y];
                 if (block is null) continue;
-                Dictionary entityData = new() {
-                    ["x"] = x,
-                    ["y"] = y,
-                    ["health"] = block.CurrentHealth,
-                    ["itemId"] = block.ItemId
-                };
-                chunkEntities.Add(entityData);
+
+                if (!groupedByItemId.ContainsKey(block.ItemId)) {
+                    groupedByItemId[block.ItemId] = new Array();
+                }
+
+                Array blockData = new() { x, y, block.CurrentHealth };
+                groupedByItemId[block.ItemId].Add(blockData);
             }
         }
 
-        return chunkEntities;
+        return groupedByItemId;
     }
 
     [Rpc(MultiplayerApi.RpcMode.AnyPeer)]
-    private void RpcProcessWorldChunk(Dictionary chunkPacket) {
-        int chunkIndex = (int)chunkPacket["chunkIndex"];
-        int totalChunks = (int)chunkPacket["totalChunks"];
-        Array blockEntities = chunkPacket["blocks"].AsGodotArray();
-        Array wallEntities = chunkPacket["walls"].AsGodotArray();
+    private void RpcProcessWorldChunk(Dictionary data) {
+        Dictionary blocksByItemId = data["blocks"].AsGodotDictionary();
+        Dictionary wallsByItemId = data["walls"].AsGodotDictionary();
 
-        foreach (Dictionary entityData in blockEntities) {
-            int x = (int)entityData["x"];
-            int y = (int)entityData["y"];
+        foreach (var kvp in blocksByItemId) {
+            ushort itemId = (ushort)kvp.Key;
+            Array blockDataArray = (Array)kvp.Value;
 
-            Blocks[x, y] = new Block {
-                CurrentHealth = (float)entityData["health"],
-                ItemId = (ushort)entityData["itemId"]
-            };
+            foreach (Array blockData in blockDataArray) {
+                int x = (int)blockData[0];
+                int y = (int)blockData[1];
+                float health = (float)blockData[2];
+
+                Blocks[x, y] = new Block {
+                    CurrentHealth = health,
+                    ItemId = itemId
+                };
+            }
         }
 
-        foreach (Dictionary entityData in wallEntities) {
-            int x = (int)entityData["x"];
-            int y = (int)entityData["y"];
+        foreach (var kvp in wallsByItemId) {
+            ushort itemId = (ushort)kvp.Key;
+            Array wallDataArray = (Array)kvp.Value;
 
-            Walls[x, y] = new Block {
-                CurrentHealth = (float)entityData["health"],
-                ItemId = (ushort)entityData["itemId"]
-            };
+            foreach (Array wallData in wallDataArray) {
+                int x = (int)wallData[0];
+                int y = (int)wallData[1];
+                float health = (float)wallData[2];
+
+                Walls[x, y] = new Block {
+                    CurrentHealth = health,
+                    ItemId = itemId
+                };
+            }
         }
 
-        // Check if this is the last chunk
-        if (chunkIndex == totalChunks - 1) {
-            SyncComplete?.Invoke();
-        }
+        SyncComplete?.Invoke();
+        
     }
 
     #endregion
