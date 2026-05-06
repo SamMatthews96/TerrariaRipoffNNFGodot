3a0e4c9775175f6b2091a36fc55df53283ed8942
Sam Matthews <57544637+SamMatthews96@users.noreply.github.com>
2026-05-07 00:54:21 +0100

    Folder restructure

--- Scripts/Resources/Item/Prop/PropButton.cs
+++ Scripts/Resources/Item/Prop/PropButton.cs
@@ -4,5 +4,9 @@ namespace TerrariaRipoffNNF;
 
 [GlobalClass]
 public partial class PropButton : PropProperty {
+    // create an element that can be clicked
+    // clicking it sends an event.
+    // TriggerManager can listen for all trigger events
+    
     
 }
